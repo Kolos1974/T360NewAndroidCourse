a165a24cfe301d7cee3cc1c899c2a80fef30b25c
Kolos1974 <karlovitz.kolos@gmail.com>
2023-02-23 15:18:49 +0100

    Initial commit

---
+++ DemoDatingApp/app/src/main/java/hu/kolos/karlovitz/demodatingapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package hu.kolos.karlovitz.demodatingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
