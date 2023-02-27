dac776f21dd47cca0435627e7f746dbe1e3cc116
Kolos1974 <karlovitz.kolos@gmail.com>
2023-02-27 21:05:08 +0100

    Added emoji rating to person detail header

--- DemoDatingApp/app/src/main/java/hu/kolos/karlovitz/demodatingapp/activity/MainActivity.kt
+++ DemoDatingApp/app/src/main/java/hu/kolos/karlovitz/demodatingapp/activity/MainActivity.kt
@@ -2,11 +2,15 @@ package hu.kolos.karlovitz.demodatingapp.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import hu.kolos.karlovitz.demodatingapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val imageView = findViewById<ImageView>(R.id.person_image)
+        imageView.setImageResource(R.drawable.clooney3)
     }
 }
