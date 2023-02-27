498efc85191866b102186e3f90de4d1b24da97fa
Kolos1974 <karlovitz.kolos@gmail.com>
2023-02-27 20:28:15 +0100

    Added person header view to details screen

--- DemoDatingApp/app/src/main/java/hu/kolos/karlovitz/demodatingapp/activity/MainActivity.kt
+++ DemoDatingApp/app/src/main/java/hu/kolos/karlovitz/demodatingapp/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package hu.kolos.karlovitz.demodatingapp
+package hu.kolos.karlovitz.demodatingapp.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import hu.kolos.karlovitz.demodatingapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
